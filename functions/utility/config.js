065eb9830616ec19b5a4833715b2cb843863dc01
Mario Rizki <mariorizki07@gmail.com>
2020-08-20 21:18:30 +0700

    done with refactoring and organizing files

---
+++ functions/utility/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyCdgFnp_RxwEptgu3OqjdL-nF0xgv3xIUU',
+  authDomain: 'psyhub-68f74.firebaseapp.com',
+  databaseURL: 'https://psyhub-68f74.firebaseio.com',
+  projectId: 'psyhub-68f74',
+  storageBucket: 'psyhub-68f74.appspot.com',
+  messagingSenderId: '769242348226',
+  appId: '1:769242348226:web:94abf01f576eed8771995b',
+  measurementId: 'G-GP9B93D4G1'
+};
